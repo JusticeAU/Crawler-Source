d94cd9ea76f2d240e9eda152c56d97c6afee4f56
Justin Etchell <justin.etchell@gmail.com>
2023-08-10 05:58:00 +1000

    Engine - Enable Mipmapping

--- Graphics/Texture.cpp
+++ Graphics/Texture.cpp
@@ -24,7 +24,8 @@ void Texture::LoadFromFile(string filename)
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 
 	// Configure this particular texture filtering
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	// clean up
